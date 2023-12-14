25fa07e323c31658d71dc2b050ee208c65446a58
eshoshix <ebohik.lol04@gmail.com>
2023-12-15 01:42:58 +0700

    Вывод инфы в редактор врачей

--- test_DataBase/EditDoctorsAcc.cs
+++ test_DataBase/EditDoctorsAcc.cs
@@ -15,7 +15,7 @@ namespace test_DataBase
     public partial class EditDoctorsAcc : Form
     {
         int currentId;
-        DataBase DataBase = new DataBase(); 
+        DataBase DataBase = new DataBase();
         public EditDoctorsAcc(int id)
         {
             currentId = id;
@@ -24,6 +24,7 @@ namespace test_DataBase
 
         private void EditDoctorsAcc_Load(object sender, EventArgs e)
         {
+            show();
             loadout();
         }
         private void loadout()
@@ -83,15 +84,65 @@ namespace test_DataBase
 
 
         }
+        private void show()
+        {
+            string queryString = $"SELECT ФИО, Специальность,[Стоимость посещения],Описание_Врача FROM Врач where ID_Врача = '{currentId}'";
+            string queryString2 = $"select count(ID_Больничного) as 'Количество приемов' from Больничные where ID_Врача = '{currentId}'";
+            SqlCommand command = new SqlCommand(queryString, DataBase.getConnection());
+            SqlCommand command2 = new SqlCommand(queryString2, DataBase.getConnection());
+            DataBase.openConnection();
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                reader.Read();
+
+                object column1Data = reader["ФИО"];
+                object column2Data = reader["Специальность"];
+                double column3Data = Convert.ToDouble(reader["Стоимость посещения"]);
+
+                object column4Data = reader["Лет"];
+                object column5Data = reader["Месяцев"];
+                string column6Data = reader["Описание_Врача"].ToString();
+
+                label1.Text = column1Data.ToString();
+                textBox1.Text = column1Data.ToString();
+                textBox2.Text = column2Data.ToString();
+                textBox3.Text = Math.Round(column3Data).ToString();
+                if (string.IsNullOrEmpty(column6Data))
+                {
+                    richTextBox1.Text = "Подробная информация отсутствует";
+                }
+                else
+                    richTextBox1.Text = column6Data.ToString();
+
+            }
+
+            
+
+
+
+
+        }
 
         private void button2_Click(object sender, EventArgs e)
         {
             UploadPhoto();
+
+            string queryString = $"SELECT ФИО, Специальность,[Стоимость посещения],Описание_Врача FROM Врач where ID_Врача = '{currentId}'";
+            string queryString2 = $"select count(ID_Больничного) as 'Количество приемов' from Больничные where ID_Врача = '{currentId}'";
+            SqlCommand command = new SqlCommand(queryString, DataBase.getConnection());
+            SqlCommand command2 = new SqlCommand(queryString2, DataBase.getConnection());
+            DataBase.openConnection();
+
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
 
+
         }
+
     }
+
+
 }
