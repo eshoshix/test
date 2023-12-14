8d32f68c69a52357c135636e290081c35a2a63d3
eshoshix <ebohik.lol04@gmail.com>
2023-12-15 05:20:08 +0700

    Отредачил форму визит

--- test_DataBase/Visit.Designer.cs
+++ test_DataBase/Visit.Designer.cs
@@ -266,6 +266,7 @@
             this.panel2.Size = new System.Drawing.Size(822, 412);
             this.panel2.TabIndex = 22;
             this.panel2.Click += new System.EventHandler(this.panel2_Click);
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
             // pictureBox1
             // 
